41cef523d41d58677a0bc8b4f7f639611e04727b
tyrpik <154254042+tyrpik@users.noreply.github.com>
2026-05-18 21:46:18 +0200

    fix: remove instanceof and type assertion code smells

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 reportWebVitals();
